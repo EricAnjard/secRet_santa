b5f4214a96c5a045caf1467820c879c41a5062cf
Xavier Laviron <xavier.laviron@gmx.fr>
2016-12-19 14:28:53 +0100

    adds verification for forbidden associations

--- picker.R
+++ picker.R
@@ -1,52 +1,95 @@
-people <- c("eric", "xavier", "guigui", "laura", "melanie", "chloe")
+# -----------------------------------------------------------------------------
+# file        : secret_picker.R
+# description : randomly choose pairs of people to organize a secret santa
+# -----------------------------------------------------------------------------
 
-secret_picker <- function(people) {
+
+secret_picker <- function(people, nt) {
 
   # get number of participants
   n_people <- length(people)
 
-  # initialize vectors
-  givers    <- people
-  receivers <- people
-  output    <- character(n_people)
+  # flag used to control if forbidden associations are made
+  flag      <- FALSE
 
-  # start main loop to choose names
-  i <- 1
-  while (i <= n_people) {
+  while (!flag) {
+    # start main loop
 
-    # choose ith giver
-    giver_i <- sample(x = givers, size = 1)
+    # initialize vectors
+    givers    <- people
+    receivers <- people
+    output    <- list()
 
-    if (i == n_people && receivers == givers ) {
-      # if loop's last turn and two vectors are the same (i.e. the giver is the
-      # receiver), reset the values to restart loop from the beginning
-      i <- 0
-      givers    <- people
-      receivers <- people
+    i <- 1
+    while (i <= n_people) {
+      # start main loop to choose names
 
-      output <- character(n_people)
-    } else {
-      # else choose ith receiver
-      while (1) {
-        # while the receiver is equal the giver, choose a receiver
-        receiver_i <- sample(x = receivers, size = 1)
-        if (receiver_i != giver_i) {
+      # choose giver_i
+      giver_i <- sample(x = givers, size = 1)
+
+      if (i == n_people && receivers == givers ) {
+        # if loop's last turn and two vectors are the same (i.e. the giver is the
+        # receiver), reset the values to restart loop from the beginning
+        i <- 0
+        givers    <- people
+        receivers <- people
+
+        output <- list()
+
+      } else {
+        # else choose receiver_i
+        while (1) {
+          # while the receiver is equal the giver, repetedly choose a receiver
+          receiver_i <- sample(x = receivers, size = 1)
+          if (receiver_i != giver_i) {
+            break
+          }
+        }
+
+        # store the receiver and the giver within output list
+        output[[i]] <- c(giver_i, receiver_i)
+
+        # pop out receiver_i and giver_i from the main vectors so they cannot
+        # be sampled again
+        givers    <- givers[givers != giver_i]
+        receivers <- receivers[receivers != receiver_i]
+      }
+
+      i <- i + 1
+    }
+
+    # now we verify if no forbidden association is made
+    # set the control flag to TRUE
+    flag <- TRUE
+
+    # we check each element of output against each element of nt, if nt
+    # specified
+    if (!is.null(nt)) {
+      for (i in 1:length(output)) {
+        for (j in 1:length(nt)) {
+          if (identical(sort(output[[i]]), sort(nt[[j]]))) {
+            # if there is a match, we toggle the flag to FALSE and exit from the
+            # inner loop
+            flag <- FALSE
+            break
+          }
+        }
+        if (!flag) {
+          # the flag is set to FALSE so we stop the outer loop and make another
+          # run of the main while loop
           break
         }
       }
-
-      # store the receiver and the giver within output vector
-      output[i] <- paste(giver_i, "gives to", receiver_i)
-
-      # pop out ith receiver and giver from the main vectors
-      givers <- givers[givers != giver_i]
-      receivers <- receivers[receivers != receiver_i]
     }
-
-    i <- i + 1
   }
 
-  print(output)
-}
+  # finally we convert the list into a data frame and output it
+  output <-
+    data.frame(matrix(unlist(output),
+                      nrow  = length(output),
+                      byrow = T),
+               stringsAsFactors = FALSE)
+  colnames(output) <- c("giver", "receiver")
 
-secret_picker(people)
+  return(output)
+}
